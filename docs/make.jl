68111fb36086c6ecd9cb06ed546e5d3d10a06c28
emirhan422 <emirhankurtulus422@gmail.com>
2020-12-28 11:33:31 +0300

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -10,11 +10,10 @@ makedocs(
     pages = Any[
         "Home" => "index.md",
         "Manual" => Any[
-            "install.md",
             "tutorial.md",
 #           "examples.md",
             "reference.md"
         ]])
 
-        
+
 deploydocs("https://github.com/emirhan422/KnetMetrics.jl.git")
